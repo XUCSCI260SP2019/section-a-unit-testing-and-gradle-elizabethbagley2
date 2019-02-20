c55ec6dcc86336e80a02e95bdf9d06ac67d17385
Elizabeth <elibagley2@gmail.com>
2019-02-19 20:24:17 -0500

    commit

--- src/main/java/edu/xavier/csci/Hello.java
+++ src/main/java/edu/xavier/csci/Hello.java
@@ -1,9 +1,9 @@
 package edu.xavier.csci;
 
-public class Hello World {
+public class Hello {
 
     public static void main(String[] args) {
-        // Prints "Hello, from csci260" to the terminal window.
+        // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
 
