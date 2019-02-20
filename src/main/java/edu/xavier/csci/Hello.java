edd74c9c6ec13e6cdbd8ee70a9c9a70879c38b2a
Elizabeth <elibagley2@gmail.com>
2019-02-19 20:23:10 -0500

    Ignoring build directory

---
+++ src/main/java/edu/xavier/csci/Hello.java
@@ -0,0 +1,10 @@
+package edu.xavier.csci;
+
+public class Hello World {
+
+    public static void main(String[] args) {
+        // Prints "Hello, from csci260" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
